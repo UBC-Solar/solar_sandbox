07a00fcf63473a2299185c1ce0d59dc024f3199c
Aarjavjain101 <Aarjavjain2736@gmail.com>
2024-08-20 23:42:41 -0700

    Feat: Abstraction created for python script structure

--- Projects/autogen_CAN_abstraction_layer/Autogenerator/autogenerator.py
+++ Projects/autogen_CAN_abstraction_layer/Autogenerator/autogenerator.py
@@ -1,41 +1,11 @@
-# Read the yaml at ../yamls/CAN_msgs
-from yaml import safe_load, YAMLError
-import os
+from utils.ParseYaml import YAML_read
 
 
-# FILE CONSTANTS
-RELATIVE_PATH           = '..'
-YAML_DIR                = 'yamls'
-CAN_MSGS_YAML           = 'CAN_msgs.yaml'
-
-FINAL_PATH = f'{RELATIVE_PATH}/{YAML_DIR}/{CAN_MSGS_YAML}'
-
-
-def _this_file_path():
-    return os.path.dirname(os.path.realpath(__file__))
-
-
-def _safe_yaml_load(file):
-    try:
-        yaml_data = safe_load(file)
-        return yaml_data
-    except YAMLError as e:
-        print(f"Error reading YAML file: {e}")
-        return None
-    
-
-
-def read_yaml():
-    # Get the directory I am running in
-    this_file_path = _this_file_path()
-    yaml_path = os.path.join(this_file_path, FINAL_PATH)
-    
-    with open(yaml_path, 'r') as file:
-        return _safe_yaml_load(file)
+# CONSTANTS
     
 
 def main():
-    print(read_yaml())
+    print(YAML_read())
 
 
 if __name__ == '__main__':
---
+++ Projects/autogen_CAN_abstraction_layer/Autogenerator/utils/ParseYaml.py
@@ -0,0 +1,34 @@
+from utils.FileUtils import _this_file_path
+from yaml import safe_load, YAMLError
+import os
+
+
+# FILE CONSTANTS
+RELATIVE_PATH           = '../..'
+YAML_DIR                = 'yamls'
+CAN_MSGS_YAML           = 'CAN_msgs.yaml'
+
+FINAL_PATH = f'{RELATIVE_PATH}/{YAML_DIR}/{CAN_MSGS_YAML}'
+
+
+
+def _safe_yaml_load(file) -> dict:
+    try:
+        yaml_data: dict = safe_load(file)
+        return yaml_data
+    except YAMLError as e:
+        print(f"Error reading YAML file: {e}")
+        return None
+    
+
+# Brief: Read the yaml specified in the yamls folder
+# Param: file_path: str - The path to the yaml file
+
+def YAML_read(file_path: str = FINAL_PATH) -> dict:
+    # Get the directory I am running in
+    this_file_path: str     = _this_file_path()
+    yaml_path: str          = os.path.join(this_file_path, file_path)
+    
+    with open(yaml_path, 'r') as file:
+        return _safe_yaml_load(file)
+    
