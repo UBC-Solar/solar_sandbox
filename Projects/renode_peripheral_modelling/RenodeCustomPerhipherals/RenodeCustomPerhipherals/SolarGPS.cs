497ddd6c5c4a8581cb83aab03b5f8bcb2028f136
Evan Owens <evancdowens@gmail.com>
2025-05-09 16:13:50 -0700

    renode i2c target

---
+++ Projects/renode_peripheral_modelling/RenodeCustomPerhipherals/RenodeCustomPerhipherals/SolarGPS.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Antmicro.Renode.Core;
+using Antmicro.Renode.Exceptions;
+using Antmicro.Renode.Logging;
+using Antmicro.Renode.Peripherals.I2C;
+using Antmicro.Renode.Peripherals.Sensor;
+using Antmicro.Renode.Utilities;
+
+namespace Antmicro.Renode.Peripherals.Miscellaneous
+{
+    public class SolarGPS : II2CPeripheral
+    {
+        private String message = "GPS!";
+        
+        public SolarGPS()
+        {
+            Reset();
+        }
+        public byte[] Read(int count = 1)
+        {
+            return System.Text.Encoding.ASCII.GetBytes(message);
+        }
+
+        public void Write(byte[] data)
+        {
+            
+        }
+        public void FinishTransmission()
+        {
+            
+        }
+
+        public void Reset()
+        {
+            return;
+        }
+    }
+}
