18694ad67e67b324e6fb17c981127896800bc190
Aarjavjain101 <Aarjavjain2736@gmail.com>
2025-05-13 23:47:32 -0700

    Setup for C

--- Projects/SoC_Algorithm/main.c
+++ Projects/SoC_Algorithm/main.c
@@ -1,12 +1,31 @@
 #include "soc.h"
+#include <stdio.h>
+
+#define HPPC_DATA_CSV   "hppc_data.csv"
+#define SOC_OUTPUT_CSV   "soc.csv"
 
 int main()
 {
-    uint8_t soc
+    FILE* f_hppc_data = fopen(HPPC_DATA_CSV, "r");
+    FILE* f_soc = fopen(SOC_OUTPUT_CSV, "w");
 
-    void SOC_predict_then_update(float g_total_pack_voltage_soc, float g_pack_current_soc, float time_step);
+    // Read line by line. CURRENT IS FIRST. then voltage
+    float current;
+    float voltage;
+    float soc;
+    while(fscanf(f_hppc_data, "%f %f", &current, &voltage) == 2)
+    {
+        // Send each of these into SoC algorithm. Output SoC into output csv
+        SOC_predict_then_update(current, voltage, SOC_TIME_STEP);
+        soc = SOC_get_soc();
 
-    SOC_get_soc
+        // Write to SoC output file
+        fprintf(f_soc, "%f\n", soc);
+    }
+
+
+    // void SOC_predict_then_update(float g_total_pack_voltage_soc, float g_pack_current_soc, float time_step);
+
 
     return 0;
 }
