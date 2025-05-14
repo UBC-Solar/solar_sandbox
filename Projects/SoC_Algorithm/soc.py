9a2dc7c6e4a63397478263f0ee293bbdaa918ab3
Aarjavjain101 <Aarjavjain2736@gmail.com>
2025-05-14 00:43:13 -0700

    Alligned data in dill

--- Projects/SoC_Algorithm/soc.py
+++ Projects/SoC_Algorithm/soc.py
@@ -35,39 +35,62 @@ from datetime import datetime
 # f.close()
 
 
-from data_tools.collections import TimeSeries
+### FSGP
 
-client = DBClient()
+f = open("fsgp_data.csv", "w")
 
-# ISO 8601-compliant times corresponding to pre-competition testing
-start = "2024-07-16T14:00:00Z"
-stop = "2024-07-17T01:00:00Z"
+with open("/home/aarjav/Documents/UBC/solar_sandbox/Projects/SoC_Algorithm/current_data_comp.dill", "rb") as pack_curr_f:
+    pack_voltage_f = open("/home/aarjav/Documents/UBC/solar_sandbox/Projects/SoC_Algorithm/voltage_data_comp.dill", "rb")
+    current_data = dill.load(pack_curr_f)
+    voltage_data = dill.load(pack_voltage_f)
 
-start_dt = datetime.fromisoformat(start)
-stop_dt  = datetime.fromisoformat(stop)
+    # for i in range(len(current_data)):
+    print(len(current_data))
+    print(len(voltage_data))
+    # for i in range(len(current_data)):
+    #     print(current_data[i], voltage_data[i], file=f)
 
-# We can, in one line, make a query to InfluxDB and parse
-# the data into a powerful format: the `TimeSeries` class.
-voltage_data: TimeSeries = client.query_time_series(
-    start=start_dt,
-    stop=stop_dt,
-    field="TotalPackVoltage",
-    units="V"
-)
-current_data: TimeSeries = client.query_time_series(
-    start=start_dt,
-    stop=stop_dt,
-    field="PackCurrent",
-    units="C"
-)
+# f.close()
 
-current_dump_f = open("current_data_comp.dill", "wb")
-voltage_dump_f = open("voltage_data_comp.dill", "wb")
-dill.dump(current_data, current_dump_f)
-dill.dump(voltage_data, voltage_dump_f)
 
-current_dump_f.close()
-voltage_dump_f.close()
+# from data_tools.collections import TimeSeries
+
+# client = DBClient()
+
+# # ISO 8601-compliant times corresponding to pre-competition testing
+# start = "2024-07-16T14:00:00Z"
+# stop = "2024-07-17T01:00:00Z"
+
+# start_dt = datetime.fromisoformat(start)
+# stop_dt  = datetime.fromisoformat(stop)
+
+# # We can, in one line, make a query to InfluxDB and parse
+# # the data into a powerful format: the `TimeSeries` class.
+# voltage_data: TimeSeries = client.query_time_series(
+#     start=start_dt,
+#     stop=stop_dt,
+#     field="TotalPackVoltage",
+#     units="V"
+# )
+# current_data: TimeSeries = client.query_time_series(
+#     start=start_dt,
+#     stop=stop_dt,
+#     field="PackCurrent",
+#     units="C"
+# )
+
+# current_data, voltage_data = TimeSeries.align(current_data, voltage_data)
+
+# current_dump_f = open("current_data_comp.dill", "wb")
+# voltage_dump_f = open("voltage_data_comp.dill", "wb")
+
+# # Align them
+
+# dill.dump(current_data, current_dump_f)
+# dill.dump(voltage_data, voltage_dump_f)
+
+# current_dump_f.close()
+# voltage_dump_f.close()
 
 # Plot the data
 # voltage_data.plot(show=False)
