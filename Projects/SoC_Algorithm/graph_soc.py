4878e921364dd67ff10ccb716c7f9e27d23cae50
Aarjavjain101 <Aarjavjain2736@gmail.com>
2025-05-14 17:25:57 -0700

    Fixed minor mistakes. Now functionaly the same as Josh's

--- Projects/SoC_Algorithm/graph_soc.py
+++ Projects/SoC_Algorithm/graph_soc.py
@@ -17,11 +17,28 @@ meta["length"] = len(voltage_data)
 meta['measurement'] = "BMS"
 meta['field'] = "SoC"
 
-soc_array = TimeSeries(np.loadtxt('soc.csv', dtype=float), meta=meta)
+meta_v = voltage_data._meta
+meta_v["start"] = voltage_data._start
+meta_v["stop"] = voltage_data._stop
+meta_v["units"] = ""
+meta_v["granularity"] = 0.1
+meta_v["length"] = len(voltage_data)
+meta_v["length"] = len(voltage_data)
+meta_v['measurement'] = "BMS"
+meta_v['field'] = "Voltage"
+
+
+soc_array = TimeSeries(np.loadtxt('/home/aarjav/Documents/UBC/physics/examples/kalman_filter_examples/soc.csv', dtype=float), meta=meta)
 voltage_data, soc_array = TimeSeries.align(voltage_data, soc_array)
 
 soc_array.plot(show=False)
 matplotlib.pyplot.savefig("image.png")
+
+# voltage_array = TimeSeries(np.loadtxt('/home/aarjav/Documents/UBC/physics/examples/kalman_filter_examples/last_voltage.csv', dtype=float), meta=meta_v)
+# voltage_data, voltage_array = TimeSeries.align(voltage_data, voltage_array)
+
+# voltage_array.plot(show=False)
+# matplotlib.pyplot.savefig("image_voltage.png")
 
 
 print(len(soc_array))
