615797b89fde021d898dede54fad46cc1097c31e
Aarjavjain101 <Aarjavjain2736@gmail.com>
2025-05-14 17:26:45 -0700

    New Main

--- Projects/SoC_Algorithm/main.c
+++ Projects/SoC_Algorithm/main.c
@@ -3,24 +3,38 @@
 
 #define HPPC_DATA_CSV   "fsgp_data.csv"
 #define SOC_OUTPUT_CSV   "soc.csv"
+#define VOLTAGE_OUTPUT_CSV   "last_voltage.csv"
+
+#define CURRENT_CSV "current.csv"
+#define VOLTAGE_CSV "voltage.csv"
 
 int main()
 {
-    FILE* f_hppc_data = fopen(HPPC_DATA_CSV, "r");
+    FILE* f_current = fopen(CURRENT_CSV, "r");
+    FILE* f_voltage = fopen(VOLTAGE_CSV, "r");
+    FILE* f_voltage_out = fopen(VOLTAGE_OUTPUT_CSV, "w");
     FILE* f_soc = fopen(SOC_OUTPUT_CSV, "w");
 
     // Read line by line. CURRENT IS FIRST. then voltage
     float current;
     float voltage;
     float soc;
-    while(fscanf(f_hppc_data, "%f %f", &current, &voltage) == 2)
+    float last_voltage;
+    fscanf(f_current, "%*[^,],%f", &current);   // Get rid of the time value
+    fscanf(f_voltage, "%*[^,],%f", &voltage);
+    while(fscanf(f_current, "%*[^,],%f", &current) == 1)
     {
+        fscanf(f_voltage, "%*[^,],%f", &voltage);
+
         // Send each of these into SoC algorithm. Output SoC into output csv
-        SOC_predict_then_update(current, voltage, SOC_TIME_STEP);
+        SOC_predict_then_update(voltage, current, SOC_TIME_STEP);
         soc = SOC_get_soc();
+        last_voltage = SOC_get_voltage();
+
 
         // Write to SoC output file
-        fprintf(f_soc, "%f\n", soc);
+        fprintf(f_soc, "%.20f\n", soc);
+        fprintf(f_voltage_out, "%.20f\n", last_voltage);
     }
 
 
